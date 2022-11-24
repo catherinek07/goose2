f82abedd74af8d61b3b0a94ac2b27e89f9a02c7f
Catherine Kim <80084168+catherinek07@users.noreply.github.com>
2022-11-23 22:19:20 -0500

    Update prototype.py

--- Code/Server/prototype.py
+++ Code/Server/prototype.py
@@ -2,6 +2,7 @@ import io
 import os
 import socket
 import struct
+import pygame
 import time
 import picamera2
 from Motor import *
@@ -11,6 +12,21 @@ from PCA9685 import PCA9685
 from Ultrasonic import *
 from Buzzer import *
 from random import *
+import sys
+#sys.path.append("/goose/local/lib/python2.7/site-packages")
+print(sys.version)
+print(sys.executable)
+print(sys.path)
+
+# from pydub import AudioSegment
+# from pydub.playback import play
+
+# song = AudioSegment.from_wav("honk-sound.mp3")
+# play(song)
+
+from playsound import *
+
+
 
 rand = Random()
 motor = Motor()
@@ -18,6 +34,10 @@ ultrasonic = Ultrasonic()
 buzzer = Buzzer()
 global turning
 turning = False
+# path = "/home/goose/Downloads/honk-sound.wav"
+
+# pygame.mixer.init()
+# honk = pygame.mixer.Sound('/home/goose/Downloads/honk-sound.wav')
 
 def turnRight(): 
     global turning 
@@ -37,7 +57,9 @@ def moveForward():
 
 def buzz(time: int): 
     Buzzer.run('1')
-    
+
+def quack():
+    playsound("/home/goose/Kitty_James/Code/Server/honk-sound.mp3")
 
 try:
     moveForward()
@@ -47,14 +69,14 @@ try:
         print("turning: " + str(turning))
         if dist < 50 and not turning:
             print("Close")
-            buzzer.run('1')
+            quack()
             if rand.randint(1,2) == 1:
                 turnRight()
             else:
                 turnLeft()
         elif dist > 70 and turning:
             print("Far")
-            buzzer.run('0')
+            # buzzer.run('0')
             moveForward()
         time.sleep(0.2)
 except KeyboardInterrupt:
